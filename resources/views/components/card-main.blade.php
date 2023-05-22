e329d164671b66a593754beb4b58049d1ed368d6
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-22 11:23:29 -0600

    Cards de destinos agregadas

---
+++ resources/views/components/card-main.blade.php
@@ -0,0 +1,10 @@
+@props(['ruta', 'title', 'price'])
+
+<div class="relative">
+    <img src="{{$ruta}}" alt="..." class="h-64 w-full rounded-3xl blur-sm" />
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center">
+        <p class=" text-4xl text-white font-semibold">{{$title}}</h1>
+            <hr class="border-white border-1 w-full mb-4">
+            <p class=" text-xl text-white font-semibold">Precio: {{$price}}</h1>
+    </div>
+</div>
--- resources/views/partials/body.blade.php
+++ resources/views/partials/body.blade.php
@@ -1,124 +1,14 @@
-{{-- <section class="gj">
-    <!-- Hero Content -->
-    <div class="bb ze ki xn 2xl:ud-px-0">
-        <div class="tc _o">
-            <div class="jn/2">
-                <h1 class="fk vj zp or kk wm wb">BIT.
-                </h1>
-                <p class="fq">
-                    Si buscas vuelos para tu próxima aventura, los encontrarás en BIT, ya sea para disfrutar de unas
-                    merecidas vacaciones, una escapada de fin de semana o para hacer ese importante viaje de negocios
-                    que estabas organizando.
-                </p>
-            </div>
-        </div>
-    </div>
-</section> --}}
-<!-- ===== Hero End ===== -->
-
-<!-- ===== Blog Start ===== -->
 <section class="ji gp uq">
-    <!-- Section Title Start -->
-    <div
-        x-data="{ sectionTitle: `Top destinos más buscados hoy en México`, sectionTitleText: `Escoge el mejor vuelo para tu próxima aventura` }">
-        <div class="bb ze rj ki xn vq">
-            <h2 x-text="sectionTitle" class="fk vj pr kk wm on/5 gq/2 bb _b">
-            </h2>
-            <p class="bb on/5 wo/5 hq" x-text="sectionTitleText"></p>
-        </div>
-
-
-    </div>
-    <!-- Section Title End -->
+    <x-title-section title="Top destinos más buscados en México" contenido="Escoge el mejor vuelo para tu próxima aventura"/>
 
     <div class="bb ye ki xn vq jb jo">
         <div class="wc qf pn xo zf iq">
-            <!-- Blog Item -->
-            <div class=" sg vk rm xm">
-                <div class="c rc i z-1 pg">
-                    <img class="w-full" src="{{asset('images/blog-01.jpg')}}" alt="Blog" />
-
-                    <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                        <a href="" class="vc ek rg lk gh sl ml il gi hi">Read More</a>
-                    </div>
-                </div>
-
-                <div class="yh">
-                    <div class="tc uf wf ag jq">
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-man.svg')}}" alt="User" />
-                            <p>Musharof Chy</p>
-                        </div>
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-calender.svg')}}" alt="Calender" />
-                            <p>21 May, 2023</p>
-                        </div>
-                    </div>
-                    <h4 class="ek tj ml il kk wm xl eq lb">
-                        <a href="blog-single.html">Vuelos a Querétaro</a>
-                    </h4>
-                </div>
-            </div>
-
-            <!-- Blog Item -->
-            <div class=" sg vk rm xm">
-                <div class="c rc i z-1 pg">
-                    <img class="w-full" src="{{asset('images/blog-02.jpg')}}" alt="Blog" />
-
-                    <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                        <a href="" class="vc ek rg lk gh sl ml il gi hi">Read More</a>
-                    </div>
-                </div>
-
-                <div class="yh">
-                    <div class="tc uf wf ag jq">
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-man.svg')}}" alt="User" />
-                            <p>Musharof Chy</p>
-                        </div>
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-calender.svg')}}"" alt=" Calender" />
-                            <p>21 May, 2023</p>
-                        </div>
-                    </div>
-                    <h4 class="ek tj ml il kk wm xl eq lb">
-                        <a href="blog-single.html">Vuelos a Puebla</a>
-                    </h4>
-                </div>
-            </div>
-
-            <!-- Blog Item -->
-            <div class=" sg vk rm xm">
-                <div class="c rc i z-1 pg">
-                    <img class="w-full" src="{{asset('images/blog-03.jpg')}}" alt="Blog" />
-
-                    <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                        <a href="" class="vc ek rg lk gh sl ml il gi hi">Read More</a>
-                    </div>
-                </div>
-
-                <div class="yh">
-                    <div class="tc uf wf ag jq">
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-man.svg')}}" alt="User" />
-                            <p>Musharof Chy</p>
-                        </div>
-                        <div class="tc wf ag">
-                            <img src="{{asset('images/icon-calender.svg')}}" alt="Calender" />
-                            <p>21 May, 2023</p>
-                        </div>
-                    </div>
-                    <h4 class="ek tj ml il kk wm xl eq lb">
-                        <a href="blog-single.html">Vuelos a Tijuana</a>
-                    </h4>
-                </div>
-            </div>
+            <x-card-main ruta="https://picsum.photos/200/300?grayscale" title="Cancun" price="1500" />
+            <x-card-main ruta="https://picsum.photos/200/300?grayscale" title="Cancun" price="1500" />
+            <x-card-main ruta="https://picsum.photos/200/300?grayscale" title="Cancun" price="1500" />
+            <x-card-main ruta="https://picsum.photos/200/300?grayscale" title="Cancun" price="1500" />
+            <x-card-main ruta="https://picsum.photos/200/300?grayscale" title="Cancun" price="1500" />
         </div>
     </div>
 </section>
-<!-- ===== Blog End ===== -->
-
-
-
-<!-- ===== CTA Start ===== -->
 
